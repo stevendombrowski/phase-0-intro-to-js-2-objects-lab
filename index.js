fa0135048aa874d50d25259d8a24e091029e9013
Steven <dombrowskis153@yahoo.com>
2021-10-14 23:45:42 -0400

    Completed assignmet

--- index.js
+++ index.js
@@ -10,11 +10,7 @@ var destructivelyUpdateEmployeeWithKeyAndValue = function(employee, key, value) 
     employee[key] = value;
     return employee;
 }
-
-
-  
- 
-  function deleteFromEmployeeByKey(object, key){
+ function deleteFromEmployeeByKey(object, key){
     var newObj =  Object.assign({},object)
     delete newObj[key]
     return newObj }
